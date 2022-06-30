6cfce04acb7edb64be98e823bf8ad86545e8842c
Konate Lamine <lkonate78@yahoo.com>
2022-06-30 14:48:36 +0200

    mise a jour version

--- src/main/java/fr/eni/encheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/encheres/bo/Utilisateur.java
@@ -68,6 +68,21 @@ public class Utilisateur {
 		this.articlesachete = articlesachete;
 	}
 
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String code_postal, String ville, String mot_de_passe, int credit, int administrateur) {
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.code_postal = code_postal;
+		this.ville = ville;
+		this.mot_de_passe = mot_de_passe;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+
 
 
 	// getters et setter
@@ -196,3 +211,204 @@ public class Utilisateur {
 	}
 
 }
+
+
+
+//package fr.eni.encheres.bo;
+//
+//import java.util.List;
+//
+//public class Utilisateur {
+//
+//	private int no_utilisateur;
+//
+//	private String pseudo;
+//
+//	private String nom;
+//
+//	private String prenom;
+//
+//	private String email;
+//
+//	private String telephone;
+//
+//	private String rue;
+//	// code_postal est un String dans les cas où par ex on tape 01 pour Laon
+//	// car il va croire que c'est 1 car 0+1 ou encore quand il y a des lettres dans
+//	// les départements
+//	private String code_postal;
+//
+//	private String ville;
+//
+//	private String mot_de_passe;
+//
+//	private int credit;
+//
+//	private int administrateur = 0;
+//
+//	private List<Article> articlesvendu;	// attribut de relation
+//
+//	private List<Article> articlesachete;	// attribut de relation
+//
+//	// constructeur
+//
+//	public Utilisateur() {
+//	}
+//
+//	public Utilisateur(int no_utilisateur, String pseudo, String nom, String prenom, String email, String telephone, String rue,
+//			String code_postal, String ville, String mot_de_passe) {
+//		this.no_utilisateur = no_utilisateur;
+//		this.pseudo = pseudo;
+//		this.nom = nom;
+//		this.prenom = prenom;
+//		this.email = email;
+//		this.telephone = telephone;
+//		this.rue = rue;
+//		this.code_postal = code_postal;
+//		this.ville = ville;
+//		this.mot_de_passe = mot_de_passe;
+//	}
+//
+//	public Utilisateur(int no_utilisateur, String pseudo, String nom, String prenom, String email, String telephone, String rue,
+//			String code_postal, String ville, String mot_de_passe, int credit, int administrateur) {
+//		this(no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe);
+//		this.credit = credit;
+//		this.administrateur = administrateur;
+//	}
+//
+//	public Utilisateur(int no_utilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+//			String rue, String code_postal, String ville, String mot_de_passe, int credit, int administrateur,
+//			List<Article> articlesvendu, List<Article> articlesachete) {
+//		this(no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur);
+//		this.articlesvendu = articlesvendu;
+//		this.articlesachete = articlesachete;
+//	}
+//
+//
+//
+//	// getters et setter
+//	public int getNo_utilisateur() {
+//		return no_utilisateur;
+//	}
+//
+//	public void setNo_utilisateur(int no_utilisateur) {
+//		this.no_utilisateur = no_utilisateur;
+//	}
+//
+//	public String getPseudo() {
+//		return pseudo;
+//	}
+//
+//	public void setPseudo(String pseudo) {
+//		this.pseudo = pseudo;
+//	}
+//
+//	public String getNom() {
+//		return nom;
+//	}
+//
+//	public void setNom(String nom) {
+//		this.nom = nom;
+//	}
+//
+//	public String getPrenom() {
+//		return prenom;
+//	}
+//
+//	public void setPrenom(String prenom) {
+//		this.prenom = prenom;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//
+//		this.email = email;
+//	}
+//
+//	public String getTelephone() {
+//		return telephone;
+//	}
+//
+//	public void setTelephone(String telephone) {
+//		this.telephone = telephone;
+//	}
+//
+//	public String getRue() {
+//		return rue;
+//	}
+//
+//	public void setRue(String rue) {
+//		this.rue = rue;
+//	}
+//
+//	public String getCode_postal() {
+//		return code_postal;
+//	}
+//
+//	public void setCode_postal(String code_postal) {
+//		this.code_postal = code_postal;
+//	}
+//
+//	public String getVille() {
+//		return ville;
+//	}
+//
+//	public void setVille(String ville) {
+//		this.ville = ville;
+//	}
+//
+//	public String getMot_de_passe() {
+//		return mot_de_passe;
+//	}
+//
+//	public void setMot_de_passe(String mot_de_passe) {
+//		this.mot_de_passe = mot_de_passe;
+//	}
+//
+//	public int getCredit() {
+//		return credit;
+//	}
+//
+//	public void setCredit(int credit) {
+//		this.credit = credit;
+//	}
+//
+//	public int getAdministrateur() {
+//		return administrateur;
+//	}
+//
+//	public void setAdministrateur(int administrateur) {
+//		this.administrateur = administrateur;
+//	}
+//
+//	public List<Article> getArticlesvendu() {
+//		return articlesvendu;
+//	}
+//
+//	public void setArticlesvendu(List<Article> articlesvendu) {
+//		this.articlesvendu = articlesvendu;
+//	}
+//
+//	public List<Article> getArticlesachete() {
+//		return articlesachete;
+//	}
+//
+//	public void setArticlesachete(List<Article> articlesachete) {
+//		this.articlesachete = articlesachete;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Utilisateur [no_utilisateur=" + no_utilisateur + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom="
+//
+//				+ prenom + ", email=" + email + ", telephone=" + telephone + ", rue=" + rue + ", code_postal="
+//
+//				+ code_postal + ", ville=" + ville + ", mot_de_passe=" + mot_de_passe + ", credit=" + credit
+//				+ ", administrateur=" + administrateur + ", articlesvendu=" + articlesvendu + ", articlesachete="
+//				+ articlesachete + "]";
+//	}
+//
+//}
--- src/main/java/fr/eni/encheres/servlet/ModifierProfil.java
+++ src/main/java/fr/eni/encheres/servlet/ModifierProfil.java
@@ -44,12 +44,6 @@ public class ModifierProfil extends HttpServlet {
 //		}
 
 		
-<<<<<<< HEAD
-		utilisateur = (Utilisateur) request.getSession().getAttribute("utilisateur");
-		request.getServletContext().getRequestDispatcher("/WEB-INF/jsp/modif_profil.jsp").forward(request, response);
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-=======
->>>>>>> cec45dcb9f53e9f81f3cd795cae731c721c72a61
 	}
 
 	/**
@@ -106,17 +100,138 @@ public class ModifierProfil extends HttpServlet {
 
 			}
 			
-<<<<<<< HEAD
-		
-			
-		//doGet(request, response);
-=======
 			// Modification de l'utilisateur en cours de session (reaffichage des valeurs saisies)
 			request.getSession().setAttribute("utilisateur", utilisateurModifie);
 			request.setAttribute("message", message);
 			getServletContext().getRequestDispatcher("/modif_profil").forward(request, response);
 
->>>>>>> cec45dcb9f53e9f81f3cd795cae731c721c72a61
 	}
-	
+
 }
+
+
+
+
+
+//package fr.eni.encheres.servlet;
+//
+//import java.io.IOException;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import fr.eni.encheres.bll.BLLException;
+//import fr.eni.encheres.bll.UserManager;
+//import fr.eni.encheres.bo.Utilisateur;
+//
+///**
+// * Servlet implementation class ModifierProfil
+// */
+//public class ModifierProfil extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//	private static final String MSG_PROFIL_MODIFIE = "Votre profil a bien été modifié !";
+//	private static final String MSG_COHERENCE_NEW_MDP = "Problème de confirmation du mot de passe";
+//	private static final String MSG_ERREUR_MDP = "Mot de passe actuel incorrect";
+//
+//	/**
+//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		
+//		//supprimer un utilisateur
+////		if(choixUtilisateur.equals("Supprimer mon compte")) {
+////			request.getSession().getAttribute("utilisateur");
+////			System.out.println(utilisateur.toString());
+//			int numeroUtilisateur = Integer.valueOf(request.getParameter("no_utilisateur"));
+//			//System.out.println(no_utilisateur);
+//			UserManager userMng = UserManager.getInstance();
+//			try {
+//				userMng.removeUser(numeroUtilisateur);
+//			} catch (BLLException e) {
+//				e.printStackTrace();
+//			}
+//			request.getSession().removeAttribute("utilisateur");
+//			String message = "Votre compte a été supprimé!";
+//			request.setAttribute("message", message);
+////			doGet(request, response);
+////		}
+//
+//		
+//<<<<<<< HEAD
+//		utilisateur = (Utilisateur) request.getSession().getAttribute("utilisateur");
+//		request.getServletContext().getRequestDispatcher("/WEB-INF/jsp/modif_profil.jsp").forward(request, response);
+//		//response.getWriter().append("Served at: ").append(request.getContextPath());
+//=======
+//>>>>>>> cec45dcb9f53e9f81f3cd795cae731c721c72a61
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		
+//		// Recuperation des donnees de la session en cours
+//		System.out.println(request.getSession().getAttribute("utilisateur").toString());
+//		Utilisateur utilisateur = (Utilisateur) request.getSession().getAttribute("utilisateur");
+//		
+//		// Recuperation des valeurs du formulaire
+//			
+//			String pseudo = request.getParameter("pseudo");
+//			String nom = request.getParameter("nom");
+//			String prenom = request.getParameter("prenom");
+//			String email = request.getParameter("email");
+//			String telephone = request.getParameter("telephone");
+//			String rue = request.getParameter("rue");
+//			String code_postal = request.getParameter("code_postal");
+//			String ville = request.getParameter("ville");
+//			String mot_de_passe = request.getParameter("mot_de_passe");
+//			String nouveauMdp = request.getParameter("nouveau_mdp");
+//			String confirmationMdp = request.getParameter("confirmation_mdp");
+//
+//			Utilisateur utilisateurModifie = new Utilisateur(utilisateur.getNo_utilisateur(),
+//														pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe,
+//														utilisateur.getCredit(), utilisateur.getAdministrateur(),
+//														utilisateur.getArticlesvendu(), utilisateur.getArticlesachete());
+//			
+//			UserManager userMng = UserManager.getInstance();
+//			String message = "";
+//			// Verification de coherence entre MdP du formulaire et MdP de l'utilisateur de la session
+//			if (! mot_de_passe.equals(utilisateur.getMot_de_passe())) {
+//				message = MSG_ERREUR_MDP;
+//			}
+//
+//			// Verification de coherence pour le nouveau MdP
+//			if (! nouveauMdp.equals(confirmationMdp)) {
+//				message = MSG_COHERENCE_NEW_MDP;
+//			}
+//
+//			if (mot_de_passe.equals(utilisateur.getMot_de_passe())
+//				&& nouveauMdp.equals(confirmationMdp)
+//				) {
+//				
+//				try {
+//					userMng.updateProfil(utilisateurModifie);
+//					message = MSG_PROFIL_MODIFIE;
+//				}
+//				catch (BLLException e) {
+//					e.printStackTrace();
+//				}
+//
+//			}
+//			
+//<<<<<<< HEAD
+//		
+//			
+//		//doGet(request, response);
+//=======
+//			// Modification de l'utilisateur en cours de session (reaffichage des valeurs saisies)
+//			request.getSession().setAttribute("utilisateur", utilisateurModifie);
+//			request.setAttribute("message", message);
+//			getServletContext().getRequestDispatcher("/modif_profil").forward(request, response);
+//
+//>>>>>>> cec45dcb9f53e9f81f3cd795cae731c721c72a61
+//	}
+//	
+//}
